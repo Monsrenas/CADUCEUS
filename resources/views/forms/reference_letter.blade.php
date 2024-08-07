835c3ba5e415f3c3731c46860a7dc779de02dbd7
William Ferrer Reyes <alcadytoo@gmail.com>
2024-08-07 01:30:10 -0500

    reference letter TEST

--- resources/views/forms/reference_letter.blade.php
+++ resources/views/forms/reference_letter.blade.php
@@ -13,6 +13,7 @@
 
     <x-slot name='content'>
         @if (isset($this->letterToView))
+            {{$this->letterToView}}
             <iframe src='{{$this->letterToView}}' id="miIframe" style="border: none;" class="w-full min-h-80"   wire:ignore >    
             </iframe>
         @endif
